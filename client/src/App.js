ed430e29f95af9e07e3c7a2ad62c94c67fb6504a
Deborah Wade <sopranosamurai@yahoo.com>
2022-12-13 18:55:03 -0800

    Setup front-end and finished Header

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import React from 'react';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// import pages & components
+import Home from './pages/Home';
+import Header from './components/Header';
+
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ApolloProvider client={client}>
+      <Router>
+        <div>
+          <Header />
+          <div className='MainPage'>
+            <Routes>
+              <Route
+              path="/"
+              element={<Home />}
+              />
+            </Routes>
+          </div>
+        </div>
+      </Router>
+    </ApolloProvider>
+  )
 }
 
 export default App;
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import styles from './Header.module.css';
+import {Link} from 'react-router-dom';
 
+let loginStatus = 'Login';
 function Header() {
-// component body here??
+// component function body here??
 
     return (
-        <header>
+        <header className={styles.header}>
+            {/* add logo here */}
             <h1>Couch Tuber</h1>
-            
+            <nav>
+                <Link className='link' to='/'>Home</Link>
+                {/* <Link className='link' to='/genres'>Genre Search</Link> */}
+                <Link className='link' to='/account'>Account</Link>
+                <Link className='link' to='/about'>About</Link>
+                <Link className='link' to='/login'>{loginStatus}</Link>
+            </nav>
         </header>
 
     );
 }
-
-
-
-
 
 export default Header;
