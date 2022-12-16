369f805b3990cc5849c19029430cecf4101d03e6
slendyG <josuegarza0608@gmail.com>
2022-12-16 03:33:15 -0800

    initialized graphQL server

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
-// process.env.MONGODB_URI || 
-mongoose.connect('mongodb://127.0.0.1:27017/moviesDB', {
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/moviesDB', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
