e32e0f09f378c49e614ce49b2cf13089b6acc7b0
Deborah Wade <sopranosamurai@yahoo.com>
2022-12-18 15:47:00 -0800

    more account work

--- client/src/pages/Account.js
+++ client/src/pages/Account.js
@@ -1,11 +1,17 @@
 import React from 'react';
 // import styles from 'Account.module.css';
 import { useQuery } from '@apollo/client';
-import {}
+import {QUERY_USER} from '../utils/queries';
 import avatarPath from '../images/tuberSmall.png';
 const userName = 'CouchPotato12';
 
 const Account = () => {
+    const {loading, data} = useQuery(QUERY_USER, {
+        fetchPolicy: "no-cache"
+    })
+
+    
+
     return (
         <div>
             <img src={avatarPath} />
