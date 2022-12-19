9d776cf65f20aaaabecee2ccb3ea783512c2dd56
CalvinBauman <calvin.bauman@icloud.com>
2022-12-19 12:07:19 -0800

    about page img

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import iconPath from '../images/squareIconSm.png'
-// import styles
+import iconPath from '../images/mainLogoSm.png'
+import logoPath from '../images/mainLogo.png'
 
 const About = () => {
     return (
         <div class='body-external'>
+            <img class="flex-container float-right" src={logoPath} />
             <div class='about-me-title'> About Us </div>
             <div class='about-me'>
                 Couch Tuber is a search platform that makes it easy to find what streaming services your favorite TV Shows, Movies, Documentaries and more are located on. Additionally, you can create an account to save infomration about your streaming services; select which streaming services you are subscribed to, view all of your renewal dates, view and compare streaming service prices and, eventually, browse all the streaming services by genre at the same time. With Couch Tuber, finding entertainment has never been easier.
