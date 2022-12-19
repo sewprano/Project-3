15f950fb202ef46dab973cc0d43d52133fce12ca
slendyG <josuegarza0608@gmail.com>
2022-12-19 01:30:06 -0800

    made more changes to write queries and mutations in typeDef.js

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,43 +2,60 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
     type User {
-        _id: ID!
-        username: String!
-        email: String!
-        password: String!
-        profile: Profile!
-        streamServices: [Stream!] 
+        _id: ID
+        username: String
+        email: String
+        password: String
+        streamServices: [Stream] 
+    }
+
+    input UserInput {
+        username: String
+        email: String
+        password: String
     }
 
     type Profile {
-        _id: ID!
-        favoriteMovie: [Movie!]
+        _id: ID
+        user: User
+        summary: String
+        favoriteMovie: [Movie]
     }
 
     type Movie {
-        title: String!
+        title: String
         director: String
         year: Int
     }
 
+    input MovieInput {
+        title: String
+        director: String
+    }
+
     type Stream {
-        name: String!
-        renewalPrice: Int!
-        renewalDate: Date!
+        name: String
+        renewalPrice: Int
+        renewalDate: Int
     },
 
+    input StreamInput {
+        name: String
+    }
+
     type Query {
-        user: User!
-        profile: Profile!
-        movie: [Movie!]
-        streamingServices: [Stream!]
+        user(ID: ID!): User
+        profile: Profile
+        movie: [Movie]
+        streamingServices: [Stream]
     }
 
     type Mutation {
-        createUser(username: String!, email: String!, password: String!) : User!
+        createUser(userInput: UserInput) : User!
         deleteUser(id: ID!): User!
         createMovie(title: String!): Movie!
-        addStreamService(name: String!, renewalPrice: Int!, renewalDate: Date!)
+        editMovie(ID: ID!,movieInput: MovieInput): Boolean
+        addStreamService(StreamInput: StreamInput): Stream!
         # maybe an add profile here
     }
 `;
